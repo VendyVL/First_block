7633385055b5cde835a5984f4f2bc05f10747f75
«VendyVL» <vedmansk@yandex.ru>
2022-07-03 23:24:22 +0300

    Это код и он работает

---
+++ ex/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] arrayStart = new string[] 
+{"Welcome", "to", "Wikipedia", "the", "free", "encyclopedia", "that", "anyone", "can", "edit"};
+
+string[] ThreeSimbols(string[] array)
+{
+    string[] arrayEnd = new string[array.Length]; //больше этого точно не может быть
+    int b = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayEnd[b] = array[i];
+            b++;
+        }
+    }
+    return arrayEnd;
+}
+
+void PrintArray (string[] array)
+{
+        for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+string[] arrayFin = ThreeSimbols(arrayStart);
+PrintArray (arrayFin);
+
+//Ну... Оно работает
